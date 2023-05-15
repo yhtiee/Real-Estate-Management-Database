6e6b561a58fca6fbec85c4bf63d920fc4c7aceab
yhtiee <99039727+yhtiee@users.noreply.github.com>
2023-05-15 22:03:25 +0100

    origin

--- Real_Estate_Management/settings.py
+++ Real_Estate_Management/settings.py
@@ -76,6 +76,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Real_Estate_Management.wsgi.application'
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
